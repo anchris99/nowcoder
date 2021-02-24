be91d68b870d9f991e609378f833b7a27331c314
anchris99 <845193703@qq.com>
2021-02-24 22:19:06 +0800

    follow completed

--- src/main/java/com/anchris/controller/HomeController.java
+++ src/main/java/com/anchris/controller/HomeController.java
@@ -5,13 +5,16 @@ import com.anchris.entity.DiscussPost;
 import com.anchris.entity.Page;
 import com.anchris.entity.User;
 import com.anchris.service.DiscussPostService;
+import com.anchris.service.LikeService;
 import com.anchris.service.UserService;
+import com.anchris.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,13 +22,16 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
 /*    @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page) {
@@ -61,10 +67,18 @@ public class HomeController {
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount",likeCount);
                 discussPosts.add(map);
             }
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+
+    @RequestMapping(path = "/error" ,method = RequestMethod.GET)
+    public String getError(){
+        return "/error/500";
     }
 }
--- src/main/resources/static/js/letter.js
+++ src/main/resources/static/js/letter.js
@@ -1,39 +1,3 @@
-// $(function(){
-// 	$("#sendBtn").click(send_letter);
-// 	$(".close").click(delete_msg);
-// });
-//
-// function send_letter() {
-// 	$("#sendModal").modal("hide");
-// 	$("#hintModal").modal("show");
-//
-// 	let toName = $("#recipient-name").val();
-// 	let content = $("#message-text").val();
-// 	$.post(
-// 		CONTEXT_PATH+"/letter/send",
-// 		{"toName":toName, "content":content},
-// 		function (data){
-// 			data = $.parseJSON(data);
-// 			if(data.code==0){
-// 				$("#hintBody").text("发送成功!");
-// 			}else{
-// 				$("#hintBody").text("data.msg");
-// 			}
-// 		}
-//
-// 	);
-// 	setTimeout(function(){
-// 		$("#hintModal").modal("hide");
-// 		location.reload();
-// 	}, 2000);
-// }
-//
-// function delete_msg() {
-// 	// TODO 删除数据
-// 	$(this).parents(".media").remove();
-// }
-
-
 $(function(){
 	$("#sendBtn").click(send_letter);
 	$(".close").click(delete_msg);
